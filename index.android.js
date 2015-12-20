bd61960cbefd4474ab299f9dadc6c1291acd9bdb
Ben O'Regan <benzy.oregan@gmail.com>
2015-12-20 15:21:17 +0000

    Works cool

--- index.android.js
+++ index.android.js
@@ -2,6 +2,10 @@ var React = require('react-native');
 var MK = require('react-native-material-kit');
 var appStyles = require('./styles');
 var DrawerLayout = require('react-native-drawer-layout');
+var Image = require('react-native-image-progress');
+var ProgressBar = require('react-native-progress/Bar');
+var Dimensions = require('Dimensions');
+var windowSize = Dimensions.get('window');
 
 var {
   StyleSheet,
@@ -29,7 +33,7 @@ var styles = Object.assign(appStyles, StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 50
+    padding: 10
   },
   blockText: {
     marginBottom: 5,
@@ -48,11 +52,7 @@ var styles = Object.assign(appStyles, StyleSheet.create({
 var benactive = React.createClass({
 
   getInitialState: function() {
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    return {
-      dataSource: ds.cloneWithRows(['Read', 'Night Mode', 'Change Font', 'Settings', 'This', 'Is', 'A', 'Test']),
-      word4u: null,
-    };
+    return {};
   },
 
   componentDidMount: function() {
@@ -77,78 +77,63 @@ var benactive = React.createClass({
     }
 
     return this.renderWord(this.state.word4u);
-
-    return (
-        <DrawerLayoutAndroid
-          renderNavigationView={() => navigationView }>
-        </DrawerLayoutAndroid>
-    );
   },
 
   renderWord: function(word) {
-
     var navigationView = (
-      <View style={styles.container}>
-        <Image
-          style={styles.thumbnail}
-          source={{uri: 'https://source.unsplash.com/category/nature/1600x900'}}
-        />
-        <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this._renderRow}
-        style={styles.listView}
-      />
+      <View style={[styles.container, {backgroundColor: '#fff'}]}>
+        <Text>Hello there!</Text>
+        <TouchableHighlight onPress={() => this.drawer.closeDrawer()}>
+          <Text>Close drawer</Text>
+        </TouchableHighlight>
       </View>
     );
 
     return (
+      <DrawerLayout
+        onDrawerSlide={(e) => this.setState({drawerSlideOutput: JSON.stringify(e.nativeEvent)})}
+        onDrawerStateChanged={(e) => this.setState({drawerStateChangedOutput: JSON.stringify(e)})}
+        drawerWidth={250}
+        ref={(drawer) => { return this.drawer = drawer  }}
+        keyboardDismissMode="on-drag"
+        renderNavigationView={() => navigationView}>
       <ScrollView
-          renderNavigationView={() => navigationView }
           automaticallyAdjustContentInsets={false}
           onScroll={() => { console.log('onScroll!'); }}
           scrollEventThrottle={200}
           style={styles.scrollView}>
+      <Image source={{ uri: 'http://source.unsplash.com/category/nature/1080x900' }} indicator={ProgressBar}
+            style={{
+              position: 'absolute',
+              left: 0,
+              top: 0,
+              width: windowSize.width,
+              height: 2000,
+            }}/>
         <View style={styles.container}>
-          <View style={MKCardStyles.card}>
-            <Text style={MKCardStyles.title}>{word.title}</Text>
-            <View  // TextView padding not handled well on Android https://github.com/facebook/react-native/issues/3233
-             style={{
-               padding : 30,
-             }}
-             >
-             <View style={styles.container}>
-             <Text style={MKCardStyles.row}>{word.verse}</Text>
-            <Text style={[MKCardStyles.content, {padding:5}]}>{word.content}</Text>
+          <View style={[MKCardStyles.card, {padding : 18, marginBottom: 10}]}>
+            <Text>{word.date}</Text>
+          </View>
+            <View style={[MKCardStyles.card, {padding : 20}]}>
+              <View style={styles.container}>
+                <Text style={MKCardStyles.row}>{word.verse}</Text>
+                <Text style={MKCardStyles.col}>{word.title}{'\n'}</Text>
+                <Text style={MKCardStyles.content}>{word.content}</Text>
+              </View>
             </View>
           </View>
-        </View>
-      </View>
     </ScrollView>
+    </DrawerLayout>
     );
   },
 
   renderLoadingView: function() {
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, {shadowRadius: 10, shadowColor : 'red'}]}>
         <Text>
           Loading Word 4 U 2day...
         </Text>
       </View>
-    );
-  },
-
-  _renderRow: function(rowData: string, sectionID: number, rowID: number) {
-    return (
-      <TouchableHighlight onPress={() => this._pressRow(rowID)}>
-        <View>
-          <View style={styles.row}>
-            <Image style={styles.thumb} source={{uri:'https://source.unsplash.com/category/nature/50x50' }} />
-            <Text style={styles.text}>
-              {rowData}
-            </Text>
-          </View>
-        </View>
-      </TouchableHighlight>
     );
   },
 
--- styles.js
+++ styles.js
@@ -1,44 +1,24 @@
 var {StyleSheet} = require('react-native');
 var {MKColor} = require('react-native-material-kit');
-var Dimensions = require('Dimensions');
-var windowSize = Dimensions.get('window');
 
 module.exports = StyleSheet.create({
   scrollView: {
-    backgroundColor: '#000000',
-    height: 300,
+    backgroundColor: 'transparent',
   },
   container: {
     flex: 1,
     alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
-    padding: 20,
+    padding: 25,
   },
   row: {
     flexDirection: 'row',
-    fontWeight: 'bold',
+    fontStyle: 'italic',
   },
   col: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
-    marginLeft: 7, marginRight: 7,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginTop: 10, marginBottom: 20,
-  },
-  legendLabel: {
-    textAlign: 'center',
-    color: '#666666',
-    marginTop: 10, marginBottom: 20,
-    fontSize: 12,
-    fontWeight: '300',
+    fontWeight: 'bold',
   },
 });
